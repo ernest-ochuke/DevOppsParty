c8ec6b4f8308ccb874a2aeb1b9eca5e73049ca99
Ernest Okosobo <okosobo.ernest@gmail.com>
2019-01-23 18:24:20 +0100

    Initial project commit

---
+++ src/main/java/com/devoppsparty/devoppsparty/DevoppspartyApplication.java
@@ -0,0 +1,14 @@
+package com.devoppsparty.devoppsparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoppspartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevoppspartyApplication.class, args);
+    }
+
+}
+
